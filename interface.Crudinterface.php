8094f8bbbc9fe74f15ba170223bacdd89b16db5c
claus-expoze <claus@multimedion.dk>
2015-03-19 14:08:10 +0100

    Added functions to interface
    Added Where, andwhere and qoute functions to interface

--- interface.Crudinterface.php
+++ interface.Crudinterface.php
@@ -5,6 +5,9 @@ interface Crudinterface{
 	public function from($table);
 	public function limit($limit);
 	public function orderby($orderby);
+	public function where($collumn,$value,$operator = '=');
+	public function andwhere($collumn,$value,$operator = '=');
+	public function quote($value);
 	public function sql($sql);
 	public function execute();
 	
